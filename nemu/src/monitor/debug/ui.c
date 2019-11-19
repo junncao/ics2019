671141f90ea768ab11b6f6cfd39fbc215449f04e
tracer-ics2019 <tracer@njuics.org>
2019-11-19 16:24:21 +0000

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:24:21 up  2:43,  1 user,  load average: 1.30, 0.82, 0.57 2366a90255059e3d7ed556b57bf57904c51d793a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,15 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args){
+  int lineNum = 1;
+  if(!args){
+    lineNum = sscanf(args, "%d ", &lineNum);
+  }
+  cpu_exec(lineNum);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,6 +55,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Execute N steps", cmd_si },
 
   /* TODO: Add more commands */
 

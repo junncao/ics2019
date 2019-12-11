9ecf4f7bac48ab1ebb8cdd4588ba60933a955b6a
tracer-ics2019 <tracer@njuics.org>
2019-12-11 23:53:30 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:53:30 up  1:14,  5 users,  load average: 0.09, 0.20, 0.22 465c621ec4f2229def7b4f9c40ef9e1fa51261f1

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -38,6 +38,7 @@ static inline uint32_t instr_fetch(vaddr_t *pc, int len) {
 
 /* Instruction Decode and EXecute */
 static inline void idex(vaddr_t *pc, OpcodeEntry *e) {
+    printf("test4");
   if (e->decode)
     e->decode(pc);
   e->execute(pc);

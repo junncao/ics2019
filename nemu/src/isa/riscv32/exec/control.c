b4de04ad064f7af4546be0c12cc01b9773697d58
tracer-ics2019 <tracer@njuics.org>
2019-12-16 22:43:21 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:43:21 up  1:19,  5 users,  load average: 0.55, 0.35, 0.29 4cd61369a00b16b47251f90f9249c380cefc44e6

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -3,5 +3,5 @@ make_EHelper(jal){
     s0 = decinfo.seq_pc;
     rtl_sr(id_dest->reg, &s0, 4);
     rtl_add(&(decinfo.jmp_pc), &s0, &id_src->val);
-    decinfo_set_jmp(true);
+    //decinfo_set_jmp(true);
 }

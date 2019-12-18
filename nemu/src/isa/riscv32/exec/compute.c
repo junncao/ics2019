032a00da95d582970bb664bab3703f67ffee426a
tracer-ics2019 <tracer@njuics.org>
2019-12-18 20:19:40 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:19:40 up 11:25,  5 users,  load average: 0.42, 0.43, 0.34 8ac289e97c6ef1b3e2e56cc1c02c51da886f2b75

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -57,6 +57,10 @@ make_EHelper(calu) {
             rtl_xor(&id_dest->val, &id_src->val, &id_src2->val);
             print_asm_template2(xor);
             break;
+      case 6:
+            rtl_xor(&id_dest->val, &id_src->val, &id_src2->val);
+            print_asm_template2(or);
+            break;
       default:
             assert(0 && "Unfinished CALU opcode");
 

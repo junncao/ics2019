98df35f3599c73d19fe80d30d1e41b27d9bfd990
tracer-ics2019 <tracer@njuics.org>
2019-12-18 16:41:16 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:41:16 up  7:46,  5 users,  load average: 0.34, 0.34, 0.30 c79166507b3997d343e3c7d07d69c8d62efb1fb

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -2,7 +2,7 @@
 #include "all-instr.h"
 
 static OpcodeEntry load_table [8] = {
-  EXW(ld, 1), EXW(ld, 2), EXW(ld, 4), EXW(ld, 1), EXW(ld, 2), EMPTY, EMPTY, EMPTY
+  EXW(ld, 1), EXW(ld, 2), EXW(ld, 4), EMPTY, EXW(ld, 1), EXW(ld, 2), EMPTY, EMPTY
 };
 
 static make_EHelper(load) {

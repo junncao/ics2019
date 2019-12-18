2292e6e795a28e48119f2e78e04c589f0395dd53
tracer-ics2019 <tracer@njuics.org>
2019-12-18 21:56:33 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:56:33 up 13:02,  5 users,  load average: 1.53, 1.47, 1.19 136e2ced9f926a6960ff2d012596118faf84052c

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -35,7 +35,7 @@ make_EHelper(bra){
             print_asm_template2(blt);
             break;
         case 5:
-            decinfo_set_jmp((id_src->val >= id_src2->val));
+            decinfo_set_jmp(((signed)id_src->val >= (signed)id_src2->val));
             print_asm_template2(bge);
             break;
         default:

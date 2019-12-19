c20cd68a1ff560ca629134564e3dce0d5a8a1bd0
tracer-ics2019 <tracer@njuics.org>
2019-12-19 08:51:44 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  08:51:44 up 23:57,  5 users,  load average: 1.26, 1.36, 1.36 d7b60342d083307edeb880e7e53f21e211cf72fc

--- nemu/src/isa/riscv32/exec/ldst.c
+++ nemu/src/isa/riscv32/exec/ldst.c
@@ -3,24 +3,29 @@
 make_EHelper(ld) {
   Instr instr = decinfo.isa.instr;
   rtl_lm(&s0, &id_src->addr, decinfo.width);
-  rtl_sr(id_dest->reg, &s0, 4);
 
   switch (decinfo.width) {
     case 4: print_asm_template2(lw); break;
     case 2:
-            if(instr.funct3>>2)
+            if(instr.funct3>>2){
                 print_asm_template2(lhu);
-            else
+            }
+            else{
+                s0 = (signed int)((signed short)s0);
                 print_asm_template2(lh);
+            }
             break;
     case 1:
-            if(instr.funct3>>2)
+            if(instr.funct3>>2){
                 print_asm_template2(lbu);
-            else
+            }
+            else{
                 print_asm_template2(lb);
+            }
             break;
     default: assert(0);
   }
+  rtl_sr(id_dest->reg, &s0, 4);
 }
 
 make_EHelper(st) {

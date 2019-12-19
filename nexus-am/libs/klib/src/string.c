ee2d2934bd8bd85a2f73fee82518d59cf77b28f4
tracer-ics2019 <tracer@njuics.org>
2019-12-19 15:47:24 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  15:47:24 up 41 min,  5 users,  load average: 0.19, 0.21, 0.22 c87b472b7d4d606668b9f41cf5ba214ce4d3ae4a

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,74 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+    int i = 0;
+    for(;s[i];i++){}
+    return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+    int i = 0;
+    while(src[i]){
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = 0;
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+    int i = 0;
+    while(src[i] && (i<n)){
+        dst[i] = src[i];
+        i++;
+    }
+    while(i<n){
+        dst[i] = 0;
+        i++;
+    }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+    int i = strlen(dst);
+    strcat((char*)(dst+i), src);
+    return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+    int i = 0;
+    for(;s1[i];i++){
+        if(s1[i]!=s2[i])
+            break;
+    }
+  return (s1[i]-s2[i]);
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+    int i = 0;
+    for(;s1[i] && (i<n);i++){
+        if(s1[i]!=s2[i])
+            break;
+    }
+    if(i==n)
+        return 0;
+    return (s1[i]-s2[i]);
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+    for(int i = 0; i < n;i++){
+        ((int *)v)[i] = c;
+    }
+    return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+    strncpy((char*)out, (char*)in, n);
+    return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+    return strncmp((char*)s1, (char*)s2, n);
 }
 
 #endif

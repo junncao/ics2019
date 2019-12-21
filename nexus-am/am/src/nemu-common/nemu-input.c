e558b566ca7e1a555506055b21cce05c673ab118
tracer-ics2019 <tracer@njuics.org>
2019-12-21 21:14:09 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:14:09 up  8:28,  5 users,  load average: 0.56, 0.46, 0.49 bab12d353e3fb0a530c4dde7292bd25641d68448

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -9,7 +9,7 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
       kbd->keycode = inl(KBD_ADDR);
-      kbd->keydown = !(!(kbd->keycode&0x8000));
+      kbd->keydown = 0;//!(!(kbd->keycode&0x8000));
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }

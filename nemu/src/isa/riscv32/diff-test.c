bb338b682dfb6bc8377cdc22e9f2ab1276a6f374
tracer-ics2019 <tracer@njuics.org>
2019-12-21 14:11:04 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:11:04 up  1:25,  5 users,  load average: 0.16, 0.18, 0.18 ac52eebef0c6a59876982dcb414b4f0d3161674e

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -2,6 +2,7 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+    /*
     CPU_state test;
     ref_difftest_getregs((void*)&test);
     for(int i = 0; i < 32;i++){
@@ -11,6 +12,15 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     }
     if(test.pc != pc)
         return false;
+        */
+    for(int i = 0; i < 32;i++){
+        if(reg_l(i)!=ref_r->gpr[i]._32){
+            return false;
+        }
+    }
+    if(ref_r->pc != pc)
+        return false;
+
     return true;
 }
 

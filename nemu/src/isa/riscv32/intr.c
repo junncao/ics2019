590ff37b4714cdbb263a845caad0a99015ee70e1
tracer-ics2019 <tracer@njuics.org>
2019-12-22 19:17:36 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:17:36 up  5:31,  5 users,  load average: 0.19, 0.28, 0.24 324711f52710e567c27b6fed9fe85ff148a84ba9

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -8,7 +8,7 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
     decinfo.isa.sepc = epc;
     decinfo.isa.scause = NO;
     decinfo.jmp_pc = decinfo.isa.stvec;
-    decinfo_set_jmp(true);
+    decinfo_set_jmp(false);
 }
 
 bool isa_query_intr(void) {

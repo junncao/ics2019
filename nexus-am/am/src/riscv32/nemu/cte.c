4d0226d0da15d8b9483893d93f321146c3889e67
tracer-ics2019 <tracer@njuics.org>
2019-12-23 19:12:24 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:12:24 up  2:34,  5 users,  load average: 0.34, 0.26, 0.18 f19bbb4815536d853d11f3a8f4ce6104377f974

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -12,7 +12,7 @@ _Context* __am_irq_handle(_Context *c) {
   printf("cause:%d\n", c->cause);
   printf("status:%d\n", c->status);
   printf("epc:0x%x\n", c->epc);
-    cmd_x();
+
   if (user_handler) {
     _Event ev = {0};
     switch (c->cause) {

e7e85bed2269afd983bc12e7fb6eb3c2a9e39c74
tracer-ics2019 <tracer@njuics.org>
2019-12-23 21:40:33 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:40:33 up  5:02,  5 users,  load average: 0.27, 0.17, 0.23 8bd73ed3750ef63a47e619dc6552b81c41ed7d1b

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -25,7 +25,7 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len) {
 
 void init_ramdisk() {
   Log("ramdisk info: start = %p, end = %p, size = %d bytes",
-      &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
+      &ramdisk_start, &ramdisk_end, &ramdisk_end - &ramdisk_start);
 }
 
 size_t get_ramdisk_size() {

049eebcefb761b87b3b9940f9796ec974fe2f78c
tracer-ics2019 <tracer@njuics.org>
2019-12-24 19:40:31 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:40:31 up  4:21,  5 users,  load average: 0.23, 0.17, 0.14 beb7bef019b4a6676054de879d2259fa9144ef97

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,5 +1,7 @@
 #include "common.h"
+#include "syscall.h"
 
+extern _Context* do_syscall(_Context*);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
       case _EVENT_YIELD:
@@ -7,6 +9,7 @@ static _Context* do_event(_Event e, _Context* c) {
           break;
       case _EVENT_SYSCALL:
           printf("Syscall!\n");
+          do_syscall(c);
           break;
     default: panic("Unhandled event ID = %d", e.event);
   }

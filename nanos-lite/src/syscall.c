10d07f99222f346599c052a46236d0c1c9097fe1
tracer-ics2019 <tracer@njuics.org>
2019-12-24 19:51:44 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:51:44 up  4:32,  5 users,  load average: 0.12, 0.20, 0.18 b4bb7b83f3aaf0515b9c799d2d63d9079868a913

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,8 +4,18 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
+      case SYS_exit:
+          _halt(a[1]);
+          break;
+      case SYS_yield:
+          _yield();
+          c->GPRx = 0;
+          break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 

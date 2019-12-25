390d26ead7678fb959880c33d165f811dd14941a
tracer-ics2019 <tracer@njuics.org>
2019-12-25 16:14:29 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:14:29 up  4:51,  5 users,  load average: 0.25, 0.44, 0.59 faa210742c8133f4b1048705e117ebb21b3d2c15

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -65,14 +65,13 @@ make_EHelper(system){
             s0 = readcsr(instr.csr);
             writecsr(instr.csr, id_src->val);
             rtl_sr(id_dest->reg, &s0, 4);
-            printf("reg[%d]:%d\n", id_dest->reg,reg_l(id_dest->reg));
             break;
         case 0b010:
             s0 = readcsr(instr.csr);
             writecsr(instr.csr, s0 | id_src->val);
-            printf("s0:%x\n",s0);
+            //printf("s0:%x\n",s0);
             rtl_sr(id_dest->reg, &s0, 4);
-            printf("reg:%d\n", reg_l(id_dest->reg));
+            //printf("reg:%d\n", reg_l(id_dest->reg));
             break;
         default:
             assert(0 && "Unfinished system op");

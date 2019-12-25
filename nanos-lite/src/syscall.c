8989c973a14bfeaf4c396fbcb7d311b1ddb00b9c
tracer-ics2019 <tracer@njuics.org>
2019-12-25 11:53:57 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:53:57 up 31 min,  5 users,  load average: 0.16, 0.12, 0.18 b87970f05bc5b1844b13d80bd074039adf16eca4

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,16 @@
 #include "common.h"
 #include "syscall.h"
 
+int do_write(int fd, const void*buf, size_t count){
+    if(fd!=1 && fd!=2){
+        return -1;
+    }
+    for(int i = 0;i < count;i++){
+        _putc(((char*)buf)[i]);
+    }
+    return count;
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -15,6 +25,9 @@ _Context* do_syscall(_Context *c) {
       case SYS_yield:
           _yield();
           c->GPRx = 0;
+          break;
+      case SYS_write:
+          c->GPRx = do_write(a[1], (void*)(a[2]), a[3]);
           break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }

18a9182264e9a48abcf23a2ac92f49f72a43baaa
tracer-ics2019 <tracer@njuics.org>
2019-12-25 17:24:15 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  17:24:15 up  6:01,  5 users,  load average: 0.48, 0.37, 0.36 691fb8add6e6f93a4485b2bd436b444e3f723124

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -2,10 +2,10 @@
 #include <assert.h>
 
 int main() {
+  printf("test1");
   FILE *fp = fopen("/share/texts/num", "r+");
   assert(fp);
 
-  printf("test1");
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);

f614927afdecd8bcd58fff5c1b47547ab76859d0
tracer-ics2019 <tracer@njuics.org>
2019-12-25 17:49:02 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  17:49:02 up  6:26,  5 users,  load average: 0.66, 0.49, 0.41 ca0ec8fefa41f38e63773c09985f65e663c4bb6e

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -56,8 +56,8 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
-  return 0;
+  int ret = _syscall_(SYS_open, fd, (intptr_t)buf, count);
+  return ret;
 }
 
 int _write(int fd, void *buf, size_t count) {

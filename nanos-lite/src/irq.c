0f77a4dfa7e41acf501064b3bbb004bad8123cce
tracer-ics2019 <tracer@njuics.org>
2019-12-25 12:05:02 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:05:02 up 42 min,  5 users,  load average: 0.34, 0.35, 0.26 3a5470a6c18c62a1f2fc93cfab256feed89c35a4

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,7 +8,7 @@ static _Context* do_event(_Event e, _Context* c) {
           printf("Self trap!\n");
           break;
       case _EVENT_SYSCALL:
-          printf("Syscall!\n");
+          //printf("Syscall!\n");
           do_syscall(c);
           break;
     default: panic("Unhandled event ID = %d", e.event);

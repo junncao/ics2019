61b2abb34dc811f9d7b14d9be2e0de5d9f9a39a1
tracer-ics2019 <tracer@njuics.org>
2019-12-25 16:32:01 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:32:01 up  5:09,  5 users,  load average: 0.58, 0.46, 0.58 6ad18abe8467b1fff550b16cf2b640155ba9f22

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -68,7 +68,6 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
     static int programBrk = 0;
-    return (void*)-1;
     if(programBrk == 0){
         programBrk = &end;
     }

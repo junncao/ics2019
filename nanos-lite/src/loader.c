a38ae9155b1138703f8f56bcb2e5e38dd441823f
tracer-ics2019 <tracer@njuics.org>
2019-12-25 16:31:17 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:31:17 up  5:08,  5 users,  load average: 0.39, 0.41, 0.57 f64384fa23709635166dfc73c972421d09720ab

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -26,11 +26,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       ramdisk_read((void*)Phdr.p_vaddr, Phdr.p_offset, Phdr.p_filesz);
   }
 
+  /*
   Elf_Phdr Phdr;
   ramdisk_read(&Phdr, Ehdr.e_phoff + 0*Ehdr.e_phentsize, sizeof(Phdr));
   int fd = fs_open(filename, 0, 0);
   fs_lseek(fd, 0, 0);
   fs_read(fd, (void*)Phdr.p_vaddr, -1);
+  */
   return Ehdr.e_entry;
 }
 

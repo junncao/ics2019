3255edb71d59eb969dcf7d2b5f38acb81ff4ac5f
tracer-ics2019 <tracer@njuics.org>
2019-12-27 22:16:34 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:16:34 up  4:50,  5 users,  load average: 0.30, 0.31, 0.33 4c4098006ef80c0aee519c8b59f75447a7f8344a

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -63,7 +63,7 @@ size_t fs_read(int fd, void *buf, size_t len){
             len = 0;
     }
     if(len==0)
-        return 0;
+        return -1;
     if(!file_table[fd].read){
         ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     }

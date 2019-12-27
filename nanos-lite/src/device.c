d9b22cd5624ed76a329bddf4696468e5dcc476a8
tracer-ics2019 <tracer@njuics.org>
2019-12-27 19:57:10 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:57:10 up  2:30,  5 users,  load average: 0.63, 0.33, 0.20 5dd0afd44042ae2e40b5cf1d6638698958812efc

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,5 +1,10 @@
 #include "common.h"
 #include <amdev.h>
+#include <am.h>
+#include <nemu.h>
+
+size_t __am_input_read(uintptr_t reg, void *buf, size_t size);
+size_t __am_timer_read(uintptr_t reg, void *buf, size_t size);
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
     for(int i = 0; i < len;i++){
@@ -17,6 +22,19 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+    _DEV_INPUT_KBD_t kbd;
+    __am_input_read(_DEVREG_INPUT_KBD, &kbd, 0);
+    char tmp[3]="kd";
+    if(kbd.keycode == _KEY_NONE){
+        _DEV_TIMER_UPTIME_t timer;
+        __am_timer_read(_DEVREG_TIMER_UPTIME, &timer, 0);
+        sprintf(buf+offset,"t %d\n", timer.lo);
+    }
+    else{
+        if(!kbd.keydown)
+            tmp[1] = 'u';
+        sprintf(buf+offset,"%s %s\n",tmp,keyname[kbd.keycode]);
+    }
   return 0;
 }
 

ec128c301356a195f54e85c7937a766372522aa4
tracer-ics2019 <tracer@njuics.org>
2019-12-27 22:13:52 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:13:52 up  4:47,  5 users,  load average: 0.37, 0.40, 0.36 226ded6913bf62e28e4ac999f946666e86181610

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -140,6 +140,7 @@ static void get_display_info() {
     sscanf(delim + 1, "%s", value);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
+    printf("%d %d\n", screen_w, screen_h);
   }
   fclose(dispinfo);
   assert(screen_w > 0 && screen_h > 0);

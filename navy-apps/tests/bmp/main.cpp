c1734ca10fe67b4306812d9f02467019f351a2bf
tracer-ics2019 <tracer@njuics.org>
2019-12-27 22:20:47 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:20:47 up  4:54,  5 users,  load average: 0.29, 0.29, 0.31 a92d2555417a9426c1d88e1bb09a636f3a6e136d

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -7,7 +7,6 @@ int main() {
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
   assert(bmp->pixels);
   NDL_OpenDisplay(bmp->w, bmp->h);
-  assert(0);
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
   NDL_Render();
   NDL_CloseDisplay();

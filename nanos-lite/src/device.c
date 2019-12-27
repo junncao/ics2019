79c88790441bb0e934ed092817c0316757a73d0b
tracer-ics2019 <tracer@njuics.org>
2019-12-27 21:50:26 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:50:26 up  4:23,  5 users,  load average: 0.41, 0.34, 0.24 4e3ce346bbd3ae0d616089452ffe885def2a509e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -47,8 +47,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-    int x = offset % screen_width();
-    int y = offset / screen_width();
+    int x = (offset/4) % screen_width();
+    int y = (offset/4) / screen_width();
     draw_rect((uint32_t*)buf, x, y, len, 1);
   return len;
 }

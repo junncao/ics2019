1d383b374ae85e5c08fa1dedb57d7dfc33788695
tracer-ics2019 <tracer@njuics.org>
2019-12-27 22:23:04 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:23:04 up  4:56,  5 users,  load average: 0.45, 0.37, 0.34 7100eccbc7edc6a882362ade4779499bfacb9d

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -43,6 +43,7 @@ static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
     strncpy(buf, dispinfo + offset, len);
+    printf("%d",buf);
   return len;
 }
 

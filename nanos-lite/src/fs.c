6d5a447026f5b517c229fa7682a907fa24079a83
tracer-ics2019 <tracer@njuics.org>
2019-12-27 21:45:21 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:45:21 up  4:18,  5 users,  load average: 0.17, 0.21, 0.18 cfb6f2f120861df514de10c586ce28223c87ae1c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,7 +68,10 @@ size_t fs_read(int fd, void *buf, size_t len){
     else{
         len = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     }
-    if(fd!=6)
+    if(fd==6)
+    {
+        printf("%s", buf);
+    }
         file_table[fd].open_offset += len;
     return len;
 }

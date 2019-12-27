8c97bd4cedb7d12610fc4592db446afe8f63e23a
tracer-ics2019 <tracer@njuics.org>
2019-12-27 22:14:47 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:14:47 up  4:48,  5 users,  load average: 0.23, 0.34, 0.34 8782595a2e00a4383d1ebd254d6c05c622c8a455

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -56,7 +56,7 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 
 size_t fs_read(int fd, void *buf, size_t len){
-    if(fd>=3 &&(file_table[fd].open_offset+len > file_table[fd].size)){
+    if(fd>=3 &&(file_table[fd].open_offset+len >= file_table[fd].size)){
         if(file_table[fd].size > file_table[fd].open_offset)
             len = file_table[fd].size - file_table[fd].open_offset;
         else

f9eadb523cfea55a5b86e6311471ae7e088c107a
tracer-ics2019 <tracer@njuics.org>
2019-12-27 20:20:21 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:20:21 up  2:53,  5 users,  load average: 0.39, 0.23, 0.18 fb46dd10432c40c076a283392f52dc1f7aa42f22

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -34,7 +34,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
-  {"/dev/events", 0, 0, 0, events_read, invalid_write},
+  {"/dev/events", 8000, 0, 0, events_read, invalid_write},
 #include "files.h"
 };
 

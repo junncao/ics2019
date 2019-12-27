334b48c5eba9a8da62c87eef1e245aec0379030b
tracer-ics2019 <tracer@njuics.org>
2019-12-27 21:49:18 +0800

    >  run U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:49:18 up  4:22,  5 users,  load average: 0.66, 0.35, 0.24 4d9c8b75e6e4bd7d7ea6c6d169fd63887443908

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -134,7 +134,6 @@ static void get_display_info() {
   screen_w = screen_h = 0;
   char buf[128], key[128], value[128], *delim;
   while (fgets(buf, 128, dispinfo)) {
-      printf("%s\n", buf);
     *(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
@@ -142,7 +141,6 @@ static void get_display_info() {
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
   fclose(dispinfo);
-  printf("%d\n%d\n", screen_w, screen_h);
   assert(screen_w > 0 && screen_h > 0);
 }
 

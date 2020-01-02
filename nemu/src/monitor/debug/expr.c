c1ef6a6d1ab0cdd17fd2625f354d05a3cb2e42ad
tracer-ics2019 <tracer@njuics.org>
2020-01-02 09:17:52 +0800

    >  compile U201611150 cyh Linux cyh-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:17:52 up 16 min,  4 users,  load average: 0.23, 0.43, 0.48 dbd7c5ef50bf0604cbb787be64e030b8b952f6e4

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -85,9 +85,9 @@ static bool make_token(char *e) {
         /*
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        position += substr_len;
         */
 
+        position += substr_len;
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
